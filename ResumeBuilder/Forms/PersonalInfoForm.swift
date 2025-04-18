aadd33eae6da1c085695c3eb57943aec6184996a
Rabin Joshi <rabin.aapl@gmail.com>
2025-04-18 16:25:00 -0600

    progress

--- ResumeBuilder/Forms/PersonalInfoForm.swift
+++ ResumeBuilder/Forms/PersonalInfoForm.swift
@@ -17,56 +17,78 @@ struct PersonalInfoForm: View {
 
     var body: some View {
         Form {
-            Section("Personal Information") {
-
-                TextField(text: $personalInfo.name, prompt: Text("Enter Name")) {
-                    Text("Name")
-                        .font(.body)
-                        .foregroundStyle(Color.secondary)
-                }
-                .focused($focusedField, equals: .name)
-                .textContentType(.name)
-
-                TextField(text: $personalInfo.title, prompt: Text("Enter Title")) {
-                    Text("Title")
-                        .font(.body)
-                        .foregroundStyle(Color.secondary)
-                }
-                .focused($focusedField, equals: .title)
-                .textContentType(.jobTitle)
-
-                TextField(text: $personalInfo.location, prompt: Text("Location")) {
-                    Text("Location")
-                        .font(.body)
-                        .foregroundStyle(Color.secondary)
-                }
-                .focused($focusedField, equals: .location)
-                .textContentType(.addressCity)
-
-                TextField(text: $personalInfo.email, prompt: Text("Email")) {
-                    Text("Email")
-                        .font(.body)
-                        .foregroundStyle(Color.secondary)
-                }
-                .focused($focusedField, equals: .email)
-                .textContentType(.emailAddress)
-
-                TextField(text: $personalInfo.phone, prompt: Text("Phone")) {
-                    Text("Phone")
-                        .font(.body)
-                        .foregroundStyle(Color.secondary)
-                }
-                .focused($focusedField, equals: .phone)
-                .textContentType(.telephoneNumber)
-            }
-
-            Section("Summary") {
-                TextEditor(text: $personalInfo.summary)
-                    .focused($focusedField, equals: .summary)
-                    .textEditorStyle(.plain)
-            }
+            PersonalInfoSection(personalInfo: personalInfo)
+            PersonalInfoSummarySection(personalInfo: personalInfo)
         }
         .formStyle(.grouped)
+    }
+
+}
+
+struct PersonalInfoSection: View {
+
+    @Bindable var personalInfo: PersonalInfo
+    @FocusState private var focusedField: FormField?
+
+    enum FormField {
+        case name, title, location, email, phone, summary
+    }
+
+
+    var body: some View {
+        Section("Personal Information") {
+
+            TextField(text: $personalInfo.name, prompt: Text("Enter Name")) {
+                Text("Name")
+                    .font(.body)
+                    .foregroundStyle(Color.secondary)
+            }
+            .focused($focusedField, equals: .name)
+            .textContentType(.name)
+
+            TextField(text: $personalInfo.title, prompt: Text("Enter Title")) {
+                Text("Title")
+                    .font(.body)
+                    .foregroundStyle(Color.secondary)
+            }
+            .focused($focusedField, equals: .title)
+            .textContentType(.jobTitle)
+
+            TextField(text: $personalInfo.location, prompt: Text("Location")) {
+                Text("Location")
+                    .font(.body)
+                    .foregroundStyle(Color.secondary)
+            }
+            .focused($focusedField, equals: .location)
+            .textContentType(.addressCity)
+
+            TextField(text: $personalInfo.email, prompt: Text("Email")) {
+                Text("Email")
+                    .font(.body)
+                    .foregroundStyle(Color.secondary)
+            }
+            .focused($focusedField, equals: .email)
+            .textContentType(.emailAddress)
+
+            TextField(text: $personalInfo.phone, prompt: Text("Phone")) {
+                Text("Phone")
+                    .font(.body)
+                    .foregroundStyle(Color.secondary)
+            }
+            .focused($focusedField, equals: .phone)
+            .textContentType(.telephoneNumber)
+        }
+    }
+}
+
+struct PersonalInfoSummarySection: View {
+    @Bindable var personalInfo: PersonalInfo
+
+    var body: some View {
+        Section("Summary") {
+            TextEditor(text: $personalInfo.summary)
+                .textEditorStyle(.plain)
+        }
     }
 
 }
